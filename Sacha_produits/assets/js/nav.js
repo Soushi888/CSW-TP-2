9252dbba5ac3ef7df1c502cac47687eb8108082b
Soushi888 <Sacha.pignot@gmail.com>
2019-12-13 03:44:57 -0500

    Integration du header et du footer responsive pour la page produit

---
+++ Sacha_produits/assets/js/nav.js
@@ -0,0 +1,9 @@
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+  document.getElementById("monMenuMobile").style.width = "350px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("monMenuMobile").style.width = "0";
+}
