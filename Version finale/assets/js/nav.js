87661ad0d0bf5d7a2e9cfdec28096cac84ec1939
Soushi888 <Sacha.pignot@gmail.com>
2019-12-19 02:13:50 -0500

    Mise à jour Finale !

--- Version finale/assets/js/nav.js
+++
@@ -1,9 +0,0 @@
-/* Set the width of the sidebar to 250px (show it) */
-function openNav() {
-  document.getElementById("monMenuMobile").style.width = "350px";
-}
-
-/* Set the width of the sidebar to 0 (hide it) */
-function closeNav() {
-  document.getElementById("monMenuMobile").style.width = "0";
-}
